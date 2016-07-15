5c2c3084c89d69a88cacf8b9ead6dc4d1a49c5af
mawa <makovskayamd@gmail.com>
2016-07-15 21:59:28 +0300

    finished kata

--- week05/add.js
+++ week05/add.js
@@ -1,11 +1,55 @@
 
 const add = str => {
+
 	if (str === "") return 0;
-	let regexp = new RegExp (/,|\n/);
-	// let delimiter = str.
-	let nums = str.split(regexp); 
-	return nums.map(str => str = parseInt(str))
-		       .reduce((a,b) => a + b);
+	//if delimiter is new line or comma
+	let delimiter = new RegExp (',|\n');
+	//if custom delimiter, str looks like '//[delimiter]\n[numbers]'
+	let searchForDelimiter = str.match(/^\/\/(.*)\n/);	// null || ['//[delimiter]\n', '[delimiter]']
+	if (searchForDelimiter){ 
+
+		delimiter = searchForDelimiter[1];
+		toCut = searchForDelimiter[0].length;
+		str = str.slice(toCut); // str without trash
+	}
+
+	if (delimiter.length > 1) {delimiter = getDelimiters(delimiter)};
+
+	//if nums are negative
+	let strArr = str.split(delimiter); 
+	//array of negative nums
+	let negatives = [];
+
+	let nums = strArr.map(str => {
+		//check if negative
+		if(str.search(/-/) !== -1){
+			negatives.push(str);
+		}
+
+		if(str > 1000) str = 0;
+
+		return parseInt(str)
+
+	})
+
+	if (negatives.length > 0){
+		 return new Error ('negatives not allowed: '+negatives.join(', '));
+	}
+
+
+
+	return nums.reduce((a,b) => a + b);
 }
 
 module.exports = add;
+
+function getDelimiters(s){
+	let delims = [];
+	while(s.length > 0){
+		let reg = new RegExp ('^('+s.charAt(0)+'*)');
+		let delim = s.match(reg)[1];
+		delims.push(delim);
+		s = s.slice(delim.length);
+	}
+	return new RegExp(delims.join('|'));
+}
--- week05/test/test.js
+++ week05/test/test.js
@@ -4,8 +4,8 @@ require("chai").should()
 
 const add = require("../add")
 
-describe("add", () =>
-{
+describe("add", () =>{
+
 	it("should return 0 for empty string", () =>{
 		add("").should.equal(0);
 	})
@@ -23,4 +23,29 @@ describe("add", () =>
 	it('should handle new lines between numbers', () =>{
 		add('1\n2\n3').should.equal(6);
 	})
+	it('should handle neew lines and commas as delimiters', () =>{
+		add('100\n2,3').should.equal(105);
+	})
+	it('should throw error if there is no number between new line and comma', () =>{
+		add('1,/n').should.be.NaN;
+	})
+	it('should handle custom delimiters', () =>{
+		add('//;\n1;2;3;4').should.equal(10);
+	})
+	it('shold throw an exception "negatives not allowed" if input is negative string', () =>{
+		add('-1,-2,0').should.throw.error;
+	})
+	it('Numbers bigger than 1000 should be ignored', () =>{
+		add('5, 1001').should.equal(5);
+	})
+	it('should handle delimiter of any length', () =>{
+		add('//;;;;\n1;;;;20;;;;3').should.equal(24);
+	})
+	it('should handle multiple custom delimiters', () =>{
+		add('//!@\n1!1@0').should.equal(2);
+	})
+	it('should handle multiple custom delimiters that are longer than one symbol', () =>{
+		add('//!!!@\n1!!!1@0').should.equal(2);
+	})
+
 })
